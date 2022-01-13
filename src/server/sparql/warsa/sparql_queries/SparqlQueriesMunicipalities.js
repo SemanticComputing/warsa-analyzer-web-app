b2a8d7c10b69de877010bb8c0333ba408fef9470
esikkala <esko.ikkala@aalto.fi>
2022-01-13 14:52:03 +0200

    Add JSON config for municipalities

--- src/server/sparql/warsa/sparql_queries/SparqlQueriesMunicipalities.js
+++ src/server/sparql/warsa/sparql_queries/SparqlQueriesMunicipalities.js
@@ -1,13 +1,26 @@
 const perspectiveID = 'municipalities'
 
 export const municipalityPropertiesFacetResults = `
-  ?id skos:prefLabel ?prefLabel__id .
-  BIND (?prefLabel__id as ?prefLabel__prefLabel)
-  BIND (?id AS ?prefLabel__dataProviderUrl)
-  #BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
-  BIND(?id as ?uri__id)
-  BIND(?id as ?uri__dataProviderUrl)
-  BIND(?id as ?uri__prefLabel)
+  {
+    ?id skos:prefLabel ?prefLabel__id .
+    BIND (?prefLabel__id as ?prefLabel__prefLabel)
+    BIND (?id AS ?prefLabel__dataProviderUrl)
+    #BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+    BIND(?id as ?uri__id)
+    BIND(?id as ?uri__dataProviderUrl)
+    BIND(?id as ?uri__prefLabel)
+  }
+  UNION 
+  {
+    ?id geosparql:sfWithin ?county__id .
+    ?county__id skos:prefLabel ?county__prefLabel .
+  }
+  UNION 
+  {
+    ?id dct:source ?source__id .
+    ?source__id skos:prefLabel ?source__prefLabel .
+    FILTER(LANG(?source__prefLabel) = '<LANG>')
+  }
 `
 
 export const municipalitiesPolygonsQuery = `
