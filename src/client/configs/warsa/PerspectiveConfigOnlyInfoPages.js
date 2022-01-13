1999dbe622ff26765724fb8f78e87a8697cb8f87
esikkala <esko.ikkala@aalto.fi>
2022-01-13 15:49:53 +0200

    Remove old configs

--- src/client/configs/warsa/PerspectiveConfigOnlyInfoPages.js
+++
@@ -1,5 +0,0 @@
-// import React from 'react'
-// import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay'
-// import CloudDownloadIcon from '@material-ui/icons/CloudDownload'
-
-export const perspectiveConfigOnlyInfoPages = []
